eedc69796d352cd298055af1fc370cb3509ec13c
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-03-23 12:04:40 +0100

    trim returned string

--- modules/list/getFieldLabel.js
+++ modules/list/getFieldLabel.js
@@ -1,5 +1,5 @@
 function getFieldLabel(id) {
     fieldElement = JotForm.getFieldFromID(id);
     fieldName = fieldElement.getElementsByTagName("label")[0].innerHTML;
-    return fieldName;
+    return fieldName.trim();
 }
