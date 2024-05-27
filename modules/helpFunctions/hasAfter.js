6712b85395767fa007b71fc293d4f0b38b226596
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-05-27 15:25:34 +0200

    Create hasAfter.js

---
+++ modules/helpFunctions/hasAfter.js
@@ -0,0 +1,7 @@
+const hasAfter = selector => {
+    console.log("hasAfter Selector", selector);
+    const el = document.querySelector(selector);
+    return getComputedStyle(el, '::after').content !== 'none';
+}
+//Ori Drori on Stackoverflow
+//https://stackoverflow.com/questions/58197145/how-to-detect-whether-an-element-has-after-pseudo-element
