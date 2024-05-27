df5696e097c77c00ecd1fb5a4a14b51bc51484c9
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-05-27 20:30:07 +0200

    Update validateNotEquals.js

--- modules/termValidation/validateNotEquals.js
+++ modules/termValidation/validateNotEquals.js
@@ -1,4 +1,29 @@
 function validateNotEquals(term) {
     console.log("Checking a notEquals term");
-    return false;
+    console.log(term);
+    fieldElement = JotForm.getFieldFromID(term.field);
+    console.log("fieldElement", fieldElement);
+    var fieldType = fieldElement.dataset.type;
+    switch (fieldType) {
+        case "control_checkbox":
+            console.log("Checking a control_checkbox");
+            let boxes = fieldElement.getElementsByClassName("form-checkbox");
+            for (var i = 0; i < boxes.length; i++){
+                console.log(boxes[i].value);
+                var afterElement = getComputedStyle(boxes[i], ':after');
+                console.log(afterElement);
+                console.log(afterElement.getPropertyValue("isConnected"));
+                console.log(afterElement.getPropertyValue("baseURI"));
+                console.log(afterElement.getPropertyValue("clientHeight"));
+                console.log(afterElement.getPropertyValue("parentElement"));
+                console.log(afterElement.getPropertyValue("tagName"));
+                if (term.value == boxes[i].value){
+                    return false;
+                }
+            }
+            //list all input type="checkbox"
+            //check term against it
+            break;
+    }
+    return true;
 }
