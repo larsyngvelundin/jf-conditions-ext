def2450d1147a212eddd90c69e4aec7c2c296c9f
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-05-27 20:29:19 +0200

    Update getFieldLabel.js

--- modules/list/getFieldLabel.js
+++ modules/list/getFieldLabel.js
@@ -1,5 +1,17 @@
 function getFieldLabel(id) {
+    try{
     fieldElement = JotForm.getFieldFromID(id);
     fieldName = fieldElement.getElementsByTagName("label")[0].innerHTML;
+    fieldName = removeSpanTags(fieldName);
+    fieldName = fieldName.trim();
+    if (fieldName == ""){
+        return "Empty Label"
+    }
     return fieldName.trim();
+    }
+    catch (error){
+        fieldName = `Unkown Label ${id}`
+        console.log("id:", id);
+        return fieldName;
+    }
 }
