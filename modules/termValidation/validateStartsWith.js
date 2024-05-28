569667c7819475a01888797c019b4261a7c3e2d2
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-05-28 17:31:44 +0200

    Added support for Email

---
+++ modules/termValidation/validateStartsWith.js
@@ -0,0 +1,31 @@
+function validateStartsWith(term) {
+    var field = JotForm.getFieldFromID(term.field);
+    var fieldType = field.dataset.type;
+    var equalToField = term.value[0] == "{" && term.value.slice(-1) == "}";
+    console.log("validateStartsWith term:", term);
+    if (equalToField) {
+        targetFieldId = JotForm.getFieldIdFromFieldRef(term.value);
+        targetField = JotForm.getFieldFromID(targetFieldId);
+        
+        switch (fieldType) {
+            case "control_email":
+                currentValue = field.getElementsByTagName("input")[0].value;
+                targetFieldValue = targetField.getElementsByTagName("input")[0].value;
+                if (targetFieldValue && currentValue.startsWith(targetFieldValue)){
+                    return true;
+                }
+                return false;
+        }
+    }
+    else {
+        switch (fieldType) {
+            case "control_email":
+                currentValue = field.getElementsByTagName("input")[0].value;
+                if (currentValue.startsWith(term.value)){
+                    return true;
+                }
+                return false;
+        }
+    }
+    return false;
+}
