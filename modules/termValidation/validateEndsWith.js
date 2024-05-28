c9126aa966207a4c72bc9de1bad26907c18158f3
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-05-28 17:45:51 +0200

    Added support for Email

---
+++ modules/termValidation/validateEndsWith.js
@@ -0,0 +1,31 @@
+function validateEndsWith(term) {
+    var field = JotForm.getFieldFromID(term.field);
+    var fieldType = field.dataset.type;
+    var equalToField = term.value[0] == "{" && term.value.slice(-1) == "}";
+    console.log("validateEndsWith term:", term);
+    if (equalToField) {
+        targetFieldId = JotForm.getFieldIdFromFieldRef(term.value);
+        targetField = JotForm.getFieldFromID(targetFieldId);
+        
+        switch (fieldType) {
+            case "control_email":
+                currentValue = field.getElementsByTagName("input")[0].value;
+                targetFieldValue = targetField.getElementsByTagName("input")[0].value;
+                if (targetFieldValue && currentValue.endsWith(targetFieldValue)){
+                    return true;
+                }
+                return false;
+        }
+    }
+    else {
+        switch (fieldType) {
+            case "control_email":
+                currentValue = field.getElementsByTagName("input")[0].value;
+                if (currentValue.endsWith(term.value)){
+                    return true;
+                }
+                return false;
+        }
+    }
+    return false;
+}
