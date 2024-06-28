01e9e53835bf13cd7bb04fc6a0ccb371b0f956e8
Lars <34173819+larsyngvelundin@users.noreply.github.com>
2024-06-28 21:49:22 +0200

    Update addHoverEffect.js

--- modules/helpFunctions/addHoverEffect.js
+++ modules/helpFunctions/addHoverEffect.js
@@ -1,32 +1,66 @@
-function addHoverEffect(className) {
-    var elms = document.getElementsByClassName(className);
-    var n = elms.length;
+function addHoverEffect(element) {
+    function getClassListStr() {
+        var classListStr = "";
+        var classList = element.classList;
+        console.log(" classListStr", classListStr);
+        classList.forEach(classEntry => {
+            console.log("classEntry", classEntry);
+            if (classEntry.indexOf("conflict-") > -1) {
+                classListStr = classListStr + "." + classEntry + ",";
+            }
+        });
+        classListStr = classListStr.slice(0, -1);
+        console.log("trying to show", classListStr);
+        return classListStr;
+    }
+
+    // try {
+    //     var elms = document.querySelectorAll(getClassListStr());
+    // }
+    // catch {
+    //     var elms = []
+    // }
+    // console.log("elms", elms);
+    // var n = elms.length;
     function addConflictClass() {
-        for (var i = 0; i < n; i++) {
-            var elms = document.getElementsByClassName(className);
-            // elms[i].style.backgroundColor = color;
-            elms[i].classList.add("conflictConditionHover")
+        try {
+            var elms = document.querySelectorAll(getClassListStr());
+            console.log("this is the elements:", elms);
+        }
+        catch {
+            var elms = []
+        }
+        for (var i = 0; i < elms.length; i++) {
+            elms[i].classList.add("conflictConditionHover");
+            if (element.tagName == 'DIV'){
+                elms[i].classList.remove("conflictConditionHover");
+            }
         }
     }
     function removeConflictClass() {
-        for (var i = 0; i < n; i++) {
-            // var elms = document.getElementsByClassName(className);
-            // elms[i].style.backgroundColor = color;
-            elms[i].classList.remove("conflictConditionHover")
-            // elms[i].style.backgroundColor = color;
+        try {
+            var elms = document.querySelectorAll(getClassListStr());
+        }
+        catch {
+            var elms = []
+        }
+        for (var i = 0; i < elms.length; i++) {
+            elms[i].classList.remove("conflictConditionHover");
         }
     }
-    for (var i = 0; i < n; i++) {
-        elms[i].onmouseover = function () {
-            // console.log(elms[i]);
-            // elms[i].classList.add(conflictConditionHover)
-            // changeColor("yellow");
-            addConflictClass();
-        };
-        elms[i].onmouseout = function () {
-            removeConflictClass();
-            // elms[i].classList.remove(conflictConditionHover)
-            // changeColor("white");
-        };
-    }
+    element.onmouseover = function () {
+        addConflictClass();
+    };
+    element.onmouseout = function () {
+        removeConflictClass();
+    };
+
+    // for (var i = 0; i < n; i++) {
+    //     elms[i].onmouseover = function () {
+    //         addConflictClass();
+    //     };
+    //     elms[i].onmouseout = function () {
+    //         removeConflictClass();
+    //     };
+    // }
 }
