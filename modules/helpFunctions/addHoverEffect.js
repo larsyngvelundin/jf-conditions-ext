caf2659dfca332f85d228a7b7224a83d5878a02b
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-06-30 19:41:42 +0200

    Update addHoverEffect.js

--- modules/helpFunctions/addHoverEffect.js
+++ modules/helpFunctions/addHoverEffect.js
@@ -2,15 +2,15 @@ function addHoverEffect(element) {
     function getClassListStr() {
         var classListStr = "";
         var classList = element.classList;
-        console.log(" classListStr", classListStr);
+        // console.log(" classListStr", classListStr);
         classList.forEach(classEntry => {
-            console.log("classEntry", classEntry);
+            // console.log("classEntry", classEntry);
             if (classEntry.indexOf("conflict-") > -1) {
                 classListStr = classListStr + "." + classEntry + ",";
             }
         });
         classListStr = classListStr.slice(0, -1);
-        console.log("trying to show", classListStr);
+        // console.log("trying to show", classListStr);
         return classListStr;
     }
 
@@ -25,16 +25,35 @@ function addHoverEffect(element) {
     function addConflictClass() {
         try {
             var elms = document.querySelectorAll(getClassListStr());
-            console.log("this is the elements:", elms);
+            // console.log("this is the elements:", elms);
         }
         catch {
             var elms = []
         }
         for (var i = 0; i < elms.length; i++) {
             elms[i].classList.add("conflictConditionHover");
-            if (element.tagName == 'DIV'){
-                elms[i].classList.remove("conflictConditionHover");
+            // if (element.tagName == 'DIV'){
+            //     elms[i].classList.remove("conflictConditionHover");
+            // }
+        }
+        if(element.classList.contains("content-infos")){
+            console.log("Hovering directly on action");
+            let conflictClass = getClassListStr().slice(1);
+            console.log("conflictClass", conflictClass);
+            let conditionsListElement = document.getElementsByClassName('listGroup-content')[0];
+            let actionElements = conditionsListElement.getElementsByClassName("content-infos");
+            for (var iActive = 0; iActive < actionElements.length; iActive++){
+                if (!actionElements[iActive].classList.contains(conflictClass)) {
+                    actionElements[iActive].classList.add("conflictConditionHoverOverride");
+                }
             }
+            let conditionElements = conditionsListElement.getElementsByClassName("mediaBox");
+            for (var iActive = 0; iActive < conditionElements.length; iActive++){
+                if (!conditionElements[iActive].classList.contains(conflictClass)) {
+                    conditionElements[iActive].classList.add("conflictConditionHoverOverride");
+                }
+            }
+            // var elms = document.querySelectorAll()
         }
     }
     function removeConflictClass() {
@@ -46,6 +65,7 @@ function addHoverEffect(element) {
         }
         for (var i = 0; i < elms.length; i++) {
             elms[i].classList.remove("conflictConditionHover");
+            elms[i].classList.remove("conflictConditionHoverOverride");
         }
     }
     element.onmouseover = function () {
